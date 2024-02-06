52a991884f6d5559cf0457cb7215a7ce4802cf6a
koodoyoon <kooodoyoon@gmail.com>
2024-02-06 11:12:24 +0900

    프로젝트 기초 공사 1

--- emotion_diary/src/pages/Edit.js
+++ emotion_diary/src/pages/Edit.js
@@ -1,8 +1,33 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "doyoon" })}>
+        qs바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
